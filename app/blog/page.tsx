9fc02792e167094b55c2d9da60c46486bd4aecfc
Denys <Denys0593@gmail.com>
2023-09-27 15:08:58 +0300

    final fix error

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   }
 
 async function getData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/postgfdgs')
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
 
   if (!response.ok) {
     throw new Error('Some fetching error')
